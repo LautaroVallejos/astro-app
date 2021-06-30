eedd6a0215a407b2ef19b6ebb998a2bafbcdb01e
Lautaro Vallejos <lautaro.vallejosk4m@gmail.com>
2021-06-30 04:52:08 -0300

    Se termino la conexion a la base de datos y se creo directorio strapp con bootstrap

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST', '127.0.0.1'),
+  port: env.int('PORT', 5050),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'f123d193ab8a2ca080c260a8b45d36f1'),
---
+++ strapp/pages/_app.js
@@ -0,0 +1,18 @@
+import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import '../styles/main.css'
+
+function MyApp({ Component, pageProps }) {
+  return (
+
+    <>
+     <head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+     </head>
+
+    <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp
